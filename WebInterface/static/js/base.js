e4258d5cc55cecc894dd1228279994e8331e7bc4
SebiCreator <Sebastian.Kaeser.public@gmail.com>
2022-07-21 19:54:48 +0200

    Optimized flushes

---
+++ WebInterface/static/js/base.js
@@ -0,0 +1,8 @@
+const closeButton = document.getElementById("close-btn")
+const f = document.getElementById("flush")
+
+
+closeButton.addEventListener('click', function() {
+	const f = document.getElementById("flush")
+	f.remove()	
+})
